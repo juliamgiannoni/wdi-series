069f0dcc6545c55271fee86810e5b7ebf0ac6fbf
Julia Yi <juliamijiyi@gmail.com>
2017-06-18 16:03:27 -0700

    Update photo styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("top-button").style.display = "block";
+    } else {
+        document.getElementById("top-button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
